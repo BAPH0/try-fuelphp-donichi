b9be8c7e6639326a1ce09c34f1062178fbe4c77a
bapho <bapho.io@gmail.com>
2016-07-15 23:28:50 +0900

    add vagrant files ( clone of https://github.com/froide-kk/vagrant-donichi-php-i386 )

---
+++ vagrant/bootstrap.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6
+yum install -y httpd
+yum install -y mysql-server
+yum install -y php
+yum install -y epel-release
+rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-6
+yum install -y phpMyAdmin
+ln -s /mnt/fuelphp /home/vagrant/fuelphp
+cp /mnt/fuelphp/vagrant/*.conf /etc/httpd/conf.d
+curl get.fuelphp.com/oil | sh
+sed -i 's/^#ServerName www.example.com:80$/ServerName localhost:80/' /etc/httpd/conf/httpd.conf
+sed -i 's/^;date.timezone =$/date.timezone = Asia\/Tokyo/' /etc/php.ini
+chkconfig httpd on
+chkconfig mysqld on
+service httpd start
+service mysqld start
+mysqladmin -u root password root
