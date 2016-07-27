730917d73964b2a968d2e653abaedb0e8bfab18e
bapho <bapho.io@gmail.com>
2016-07-28 04:21:17 +0900

    delete ip input form

--- fuelphp/fuel/app/views/request/_form.php
+++ fuelphp/fuel/app/views/request/_form.php
@@ -8,12 +8,6 @@
 
 		</div>
 		<div class="form-group">
-			<?php echo Form::label('Ip', 'ip', array('class'=>'control-label')); ?>
-
-				<?php echo Form::input('ip', Input::post('ip', isset($request) ? $request->ip : ''), array('class' => 'col-md-4 form-control', 'placeholder'=>'Ip')); ?>
-
-		</div>
-		<div class="form-group">
 			<label class='control-label'>&nbsp;</label>
 			<?php echo Form::submit('submit', 'Save', array('class' => 'btn btn-primary')); ?>		</div>
 	</fieldset>
