8d54f1beee3d8c36804733009a889c510733d84f
bapho <bapho.io@gmail.com>
2016-07-27 23:09:55 +0900

    override template

---
+++ fuelphp/fuel/app/logs/2016/07/27.php
@@ -0,0 +1,3 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+ERROR - 2016-07-27 22:54:40 --> Error - Could not find asset: small-business.css in /mnt/fuelphp/fuel/core/classes/asset/instance.php on line 413
