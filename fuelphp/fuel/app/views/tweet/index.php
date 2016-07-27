14170d429b4460263c7d2fb0b52e74cf513d8423
bapho <bapho.io@gmail.com>
2016-07-27 23:12:38 +0900

    override index page

--- fuelphp/fuel/app/views/tweet/index.php
+++ fuelphp/fuel/app/views/tweet/index.php
@@ -1,5 +1,5 @@
-<ul class="nav nav-pills">
-	<li class='<?php echo Arr::get($subnav, "index" ); ?>'><?php echo Html::anchor('tweet/index','Index');?></li>
-
-</ul>
-<p>Index</p>
+<div class="row">
+  <div class="col-md-8">
+    <img src="/assets/img/photo.png" alt="" class="img-responsive img-rounded">
+  </div>
+</div>
